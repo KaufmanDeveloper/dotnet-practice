7fb5fa3b4f776d8b1bb85ccbb26950a93f7bbc97
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-06-12 13:31:35 -0400

    feat: Write rewrite custom middleware

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
 
 var todos = new List<Todo>();
 
--- MyNewApp/obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
+++ MyNewApp/obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f3ae76ab45b40fde5dff24fc723edd89260683")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2528915dc945ebe43f26ce4e9d69b4d719a3b810")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
